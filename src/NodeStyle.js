02819675b5ee1432abde80703f655d4ad96cf833
jgerber <jlgerber@gmail.com>
2019-08-11 23:19:51 -0700

    default styling

--- src/NodeStyle.js
+++ src/NodeStyle.js
@@ -1,13 +1,13 @@
 const nodeStyle = {
-  nodeBgColor: "white",
-  nodeEdgeColor: "black",
-  selNodeBgColor: "#ffcb6b",
-  selNodeEdgeColor: "red",
-  edgeColor: "black",
-  selEdgeColor: "red",
-  selIncomingEdgeColor: "red",
+  nodeBgColor: "#6c6c6c",
+  nodeEdgeColor: "white",
+  selNodeBgColor: "rgb(0, 92, 170)",
+  selNodeEdgeColor: "#0082f0",
+  edgeColor: "white",
+  selEdgeColor: "#0082f0",
+  selIncomingEdgeColor: "#0082f0",
   selStrokeWidth: "2px",
-  strokeWidth: "1px"
+  strokeWidth: "1.5px"
 };
 
 export default nodeStyle;
